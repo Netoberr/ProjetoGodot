0fbbf5d79ef53ee28c760d1914ad35acdcae7bc8
RafaBallerini <rafaella_ballerini@hotmail.com>
2020-05-04 18:49:15 -0300

    Link do botão comeback no mesmo script da cena settings

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -8,15 +8,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	$music.play()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
 func _on_comeback_pressed():
 	get_tree().change_scene("res://scenes/menu.tscn")
 	pass # Replace with function body.
